51e21b4cb124ea3983652fd2072ef750e24b4873
gcaso <76.giuseppe.caso@gmail.com>
2023-09-29 20:43:48 +0200

    feature: add tailwind base style to app layout

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './app/ui/App.vue'
 import router from './app/router'
 
+import '@/assets/index.scss';
 const app = createApp(App)
 
 app.use(createPinia())
