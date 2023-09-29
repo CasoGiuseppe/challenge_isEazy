814f65788bef38a18b887911e8c9614a68281a82
gcaso <76.giuseppe.caso@gmail.com>
2023-09-29 20:05:30 +0200

    feature: add screens and spacing tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue, js, ts, jsx, tsx}'],
   theme: {
+    screens: {
+      phone: { max: '36rem' },
+      'tablet-pt': '48rem',
+      'tablet-ld': '62rem',
+      desktop: '75rem',
+      'large-desktop': '92.5rem'
+    },
+    spacing: {
+      none: '0px',
+      xsm: '.25rem', // 4
+      sm: '.5rem', // 8
+      md: '1rem', // 16
+      lg: '1.5rem', // 24
+      xlg: '2rem', // 32
+      xxlg: '4rem' // 64
+    },
+    colors: {},
     extend: {}
   },
   plugins: []
