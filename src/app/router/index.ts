e215975cce23f234ca4e6ff73f11363ebde625e9
gcaso <76.giuseppe.caso@gmail.com>
2023-10-09 15:42:40 +0200

    feature: add login route

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -3,11 +3,25 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: null
-    // },
+    {
+      path: '/',
+      name: 'start',
+      redirect: () => {
+        return { name: 'login' };
+      }
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      meta: { type: 'login' },
+      components: {
+        default: () => import(/* webpackChunkName: "login" */ '@views/aggregator-list/AggregatorList.vue'),
+      },
+      props: {
+        default: true,
+      },
+    }
   ]
 });
 
