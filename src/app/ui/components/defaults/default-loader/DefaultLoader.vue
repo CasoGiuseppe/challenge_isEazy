9e872846c0f20d67a09b22d36b94546422e98157
gcaso <76.giuseppe.caso@gmail.com>
2023-10-09 10:17:46 +0200

    feature: add default loader component stories

--- src/app/ui/components/defaults/default-loader/DefaultLoader.vue
+++ src/app/ui/components/defaults/default-loader/DefaultLoader.vue
@@ -1,7 +1,21 @@
 <template>
-  <aside class="default-loader"><ClockIcon />wait a moment please...</aside>
+  <aside class="default-loader">
+    <ClockIcon />
+    <!-- @slot Default slot laoding message -->
+    <slot>wait a moment please...</slot>
+  </aside>
 </template>
 <script setup lang="ts">
+import { type PropType } from 'vue';
 import { ClockIcon } from '@heroicons/vue/24/solid';
+const { id } = defineProps({
+  /**
+   * Set the unique id of the default loading message
+   */
+  id: {
+    type: String as PropType<string>,
+    default: 'errorID'
+  }
+});
 </script>
 <style lang="scss" src="./DefaultLoader.scss" scoped />
