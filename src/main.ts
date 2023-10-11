04d72dffb23db1558395eca441bfb5b77361f895
gcaso <76.giuseppe.caso@gmail.com>
2023-10-11 13:35:03 +0200

    feature: create main provide for upload attach composable

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import '@assets/index.scss';
 // import stores
 import { useUserStore } from '@shared/stores/user';
 import { useMessageStore } from '@shared/stores/messages';
+import { useAttachStore} from '@shared/stores/attaches';
 
 // import composables
 import useUserInfo from '@shared/composables/useUserInfo';
@@ -21,6 +22,7 @@ import userAsyncComponent from '@shared/composables/useAsyncComponent';
 import type { IAsyncComponent } from '@shared/composables/interfaces/useAsyncComponent';
 import useObserver from '@shared/composables/useObserver';
 import type { IObserver } from '@shared/composables/interfaces/useObserver';
+import useUploadAttach from '@shared/composables/useUploadAttach';
 
 // import services
 import { HTTPService } from '@shared/providers/http/http.service';
@@ -29,6 +31,7 @@ const UseUserInfo = useUserInfo(useUserStore, new HTTPService());
 const UseMessages = useMessages(useMessageStore, new HTTPService());
 const UseAsyncComponent = userAsyncComponent();
 const UseObserver = useObserver();
+const UseUploadAttach = useUploadAttach(useAttachStore, new HTTPService())
 
 // create lazy APP
 const app = createApp(defineAsyncComponent(() => import('@ui/App.vue')));
@@ -41,4 +44,5 @@ router.isReady().then(() => app.mount("#app"));
 app.provide<IUserInfo>('UseUserInfo', UseUserInfo);
 app.provide<IMessagesDetails>('UseMessages', UseMessages);
 app.provide<IAsyncComponent>('UseAsyncComponent', UseAsyncComponent);
-app.provide<IObserver>('UseObserver', UseObserver)
+app.provide<IObserver>('UseObserver', UseObserver);
+app.provide<any>('UseUploadAttach', UseUploadAttach)
