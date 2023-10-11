be0be48d5db11fd485cfccc89e6f682e976bc18d
gcaso <76.giuseppe.caso@gmail.com>
2023-10-11 12:39:53 +0200

    feature: create attaches mock

---
+++ src/server/__mocks__/attaches.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: '90d115de-5472-5698-b872-0e821faf29cb',
+    item: {
+      version: "1",
+      title: 'Pariatur dolor sunt sunt veniam et.',
+      info: 'info',
+      url: 'http://fakeurl.com',
+      date: new Date("2023-06-24T23:24:00")
+    }
+  },
+]
--- src/server/__mocks__/messages.ts
+++ src/server/__mocks__/messages.ts
@@ -9,16 +9,6 @@ export default [
     }
   },
   {
-    id: '90d115de-5472-5698-b872-0e821faf29cb',
-    item: {
-      version: "1",
-      title: 'Pariatur dolor sunt sunt veniam et.',
-      info: 'info',
-      url: 'http://fakeurl.com',
-      date: new Date("2023-06-24T23:24:00")
-    }
-  },
-  {
     id: '90d115de-5472-4898-b872-0e821faf29cb',
     user: 'TEACHER',
     picture: 'https://api.dicebear.com/7.x/bottts/svg?seed=Ginger',
