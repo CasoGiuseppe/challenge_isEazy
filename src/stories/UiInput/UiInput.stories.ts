77f392d92468d594d165aed561dd157295e92b32
gcaso <76.giuseppe.caso@gmail.com>
2023-10-12 22:42:01 +0200

    refactor: add slots and events on base input stories

--- src/stories/UiInput/UiInput.stories.ts
+++ src/stories/UiInput/UiInput.stories.ts
@@ -9,13 +9,16 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {
     id: { control: 'text' },
-    modelValue: { control: 'text'},
-    placeholder: { control: 'text'},
+    modelValue: { control: 'text' },
+    placeholder: { control: 'text' },
     type: { control: 'select', options: Object.values(Fields) },
     input: { control: 'select', options: Object.values(Types) },
-    required: { control: 'radio', options: [true, false]},
-    disabled: { control: 'radio', options: [true, false]},
-    error: { control: 'text' }
+    required: { control: 'radio', options: [true, false] },
+    disabled: { control: 'radio', options: [true, false] },
+    hidden: { control: 'radio', options: [true, false] },
+    accept: { control: 'text' },
+    error: { control: 'text' },
+    label: { control: 'text' }
   },
   args: {
     id: 'defaultID',
@@ -25,9 +28,12 @@ const meta = {
     input: Types.TEXT,
     required: true,
     disabled: false,
-    error: 'error message'
+    hidden: false,
+    accept: '*',
+    error: '',
+    label: 'label'
   }
-} satisfies Meta<typeof BaseUiInput>
+} satisfies Meta<typeof BaseUiInput>;
 
 export default meta;
 
@@ -36,13 +42,18 @@ type Story = StoryObj<typeof BaseUiInput>;
 const Template: Story = {
   render: (args) => ({
     components: { BaseUiInput },
-    setup() { return { args }},
-    template: `<base-ui-input v-bind="args" @input="action"><template #error>{{ args.error }}</template></base-ui-input>`,
-    methods: { action: action('input') }
+    setup() {
+      return { args };
+    },
+    template: `<base-ui-input v-bind="args" @input="input" @change="change" @click="click">
+      <template #label>{{ args.label }}</template>
+      <template #error>{{ args.error }}</template>
+    </base-ui-input>`,
+    methods: { input: action('input'), change: action('change'), click: action('click') }
   })
-}
+};
 
 export const Default: Story = {
   ...Template,
   args: {}
-}
+};
